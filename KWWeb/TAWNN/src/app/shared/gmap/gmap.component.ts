2d6974a9b3b6f58382373ad0e01a997e13cbd8e2
SharpDog <5h4rpd0g@gmail.com>
2017-09-12 11:37:49 -0400

    adding auto-complete

--- KWWeb/TAWNN/src/app/shared/gmap/gmap.component.ts
+++ KWWeb/TAWNN/src/app/shared/gmap/gmap.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {} from 'googlemaps';
+import {} from '@types/googlemaps';
+import { MapsAPILoader } from '@agm/core';
 
 declare var google: any;
 
@@ -7,21 +11,64 @@ declare var google: any;
   templateUrl: './gmap.component.html',
   styleUrls: ['./gmap.component.css']
 })
-export class GmapComponent implements OnInit {
 
-  constructor() { }
+export class GmapComponent implements OnInit {
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
 
   ngOnInit() {
+    // set google maps defaults
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
 
-    var lat = 40.03287778;
-    var lng = -105.5464028;
+    // create search FormControl
+    this.searchControl = new FormControl();
 
-    var mapProp = {
-      center: new google.maps.LatLng(lat, lng),
-      zoom: 5,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById('agm-gmap'), mapProp);
+    // set current position
+    this.setCurrentPosition();
+
+    // load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          // get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          // set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
   }
 
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
 }
--- KWWeb/TAWNN/src/app/shared/shared.module.ts
+++ KWWeb/TAWNN/src/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoaderComponent } from './loader/loader.component';
-
 
 import { AgmCoreModule, MapsAPILoader } from '@agm/core';
 import { GmapComponent } from './gmap/gmap.component';
 
 @NgModule({
   imports: [
-    AgmCoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBFkcGyL4LHxNY_UN6TCVDOBnEVX_amDZI',
+      libraries: ['places']
+    }),
+
     CommonModule
   ],
   declarations: [
